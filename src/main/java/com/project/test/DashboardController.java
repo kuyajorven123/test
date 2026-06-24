cb57bd1bae5093f86e114a2f57ec1edd577277a5
kuyajorven123 <kuyajorven@gmail.com>
2026-06-24 14:44:00 +0800

    Initial commit

---
+++ src/main/java/com/project/test/DashboardController.java
@@ -0,0 +1,15 @@
+package com.project.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping({"/","/dashboard"})
+        public String dashboard(){
+            return "pages/dashboard";
+        }
+
+
+}
