dd8a0e22a6efb1c8076147e4b022e5e1ff203920
kuyajorven123 <kuyajorven@gmail.com>
2026-07-02 16:02:27 +0800

    --

--- src/main/java/com/project/test/SecurityConfig.java
+++ src/main/java/com/project/test/SecurityConfig.java
@@ -2,14 +2,31 @@ package com.project.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails user = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
