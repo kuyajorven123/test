a2acf2493bc2b4e87f08254de9abede8cc3d8d7e
kuyajorven123 <kuyajorven@gmail.com>
2026-07-14 16:58:44 +0800

    --

--- src/main/java/com/project/test/controller/AdminController.java
+++ src/main/java/com/project/test/controller/AdminController.java
@@ -7,44 +7,42 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
-
-
 
 import com.project.test.model.User;
 import com.project.test.model.UserRepository;
 
 @Controller
 public class AdminController {
-    
+
     @Autowired
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
 
-    public AdminController(UserRepository userRepository, PasswordEncoder passwordEncoder){
+    public AdminController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @GetMapping({"/admin"})
-        public String admin(Model model){
-            model.addAttribute("activePage", "admin");
-            model.addAttribute("user", userRepository.findByStatusAndRole("Active", "ADMIN"));
-            // model.addAttribute("employee", userRepository.findByRole("ADMIN"));
-            return "pages/admin";
-        }
-    
+    @GetMapping({ "/admin" })
+    public String admin(Model model) {
+        model.addAttribute("activePage", "admin");
+        model.addAttribute("user", userRepository.findByStatusAndRole("Active", "ADMIN"));
+        // model.addAttribute("employee", userRepository.findByRole("ADMIN"));
+        return "pages/admin";
+    }
 
     // create
-    @PostMapping({"/create_admin"})
-    public String create_admin(@ModelAttribute User user){
-        Optional<User> existingUser =
-                userRepository.findByEmail(user.getEmail());
+    @PostMapping({ "/create_admin" })
+    public String create_admin(@ModelAttribute User user) {
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
 
-        if (existingUser.isPresent()){
-            return "redirect:/active_employee?error";
+        if (existingUser.isPresent()) {
+            return "redirect:/admin?error";
         }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -53,4 +51,47 @@ public class AdminController {
         userRepository.save(user);
         return "redirect:/admin?success";
     }
+
+    // id getter
+    @GetMapping({ "/admin/{id}" })
+    @ResponseBody
+    public User getUser(@PathVariable Long id) {
+
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Admin not found"));
+    }
+
+    // update
+    @PostMapping({ "/update" })
+    public String update(@ModelAttribute User user) {
+
+        User existing = userRepository.findById(user.getId())
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+
+        // email checker
+        Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
+
+        if (existingUser.isPresent()
+                && !existingUser.get().getId().equals(user.getId())) {
+
+            return "redirect:/active_employee?error";
+        }
+
+        existing.setFirstname(user.getFirstname());
+        existing.setMiddlename(user.getMiddlename());
+        existing.setLastname(user.getLastname());
+        existing.setAddress(user.getAddress());
+        existing.setBirthday(user.getBirthday());
+        existing.setPosition(user.getPosition());
+        existing.setEmail(user.getEmail());
+
+        if (user.getPassword() != null && !user.getPassword().isBlank()) {
+            existing.setPassword(
+                    passwordEncoder.encode(user.getPassword()));
+        }
+
+        userRepository.save(existing);
+        return "redirect:/active_employee?update_success";
+    }
+
 }
